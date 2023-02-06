fd48aea7f03d5733eaf9882cc89aaf1d3f9748c0
Sobithnikhil <113330273+Sobithnikhil@users.noreply.github.com>
2023-02-06 11:07:23 +0530

    Add files via upload

---
+++ 13)crypt-arithmetic.py
@@ -0,0 +1,15 @@
+from re import sub
+def solve(q):
+    n=(i for i in q if i.isalpha()).next()
+    except stopIteration:
+        returnq if eval(su(r'([^0-9]+)',r'\1\2',q))else false
+    else:
+        for i in (str(i) for i in range(10) if str(i)- not in q):
+            r=solve(q.replace(n,str(i)))
+            if r:
+                return r
+            return false
+    if_name_=="_main_":
+        query="ABCDE*A==EEEEEE"
+        r=solve(query)
+        print r if else "NO SOLUTION FOUND."
